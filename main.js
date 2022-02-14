ff080f4b0ed29c0571b164a8bb04cc5bf5d17f78
Your Name <samtshiek@yahoo.fr>
2022-02-14 15:47:54 -0800

    Updated input fields and array

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ let auctionItemArray = [];
 let selectedType = "";
 
 // define a constructor to create movie objects
-let auctionItem = function (pTitle, pYear, pType) {
+let auctionItem = function (pTitle, pArtist, pYear, pPrice, pType) {
     this.title = pTitle;
+    this.artist = pArtist;
     this.year = pYear;
+    this.price = pPrice;
     this.type = pType;
 }
 
@@ -14,10 +16,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("buttonAdd").addEventListener("click", function () {
 
-        auctionItemArray.push(new auctionItem(document.getElementById("artTitle").value, document.getElementById("artYear").value, selectedType));
+        auctionItemArray.push(new auctionItem(document.getElementById("artTitle").value, document.getElementById("artist").value, document.getElementById("artYear").value, document.getElementById("artPrice").value, selectedType));
         console.log(auctionItemArray);
         document.getElementById("artTitle").value = "";
+        document.getElementById("artist").value = "";
         document.getElementById("artYear").value = "";
+        document.getElementById("artPrice").value = "";
     });
 
     $(document).bind("change", "#select-type", function (event, ui) {
@@ -44,7 +48,7 @@ function createList() {
 
     auctionItemArray.forEach(function (element,) {   // use handy array forEach method
         var li = document.createElement('li');
-        li.innerHTML = element.title + ", " + element.year + ":  " + element.type;
+        li.innerHTML = "$" + element.price + " Art type: " + element.type + " <cite>" + element.title + "</cite>, created by Artist: " + element.artist + " in year " + element.year;
         myul.appendChild(li);
     });
 };
